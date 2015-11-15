7d9d5294fa835f461ead8b56c7f593b328337bbf
galaktika81 <galaktika81@gmail.com>
2015-11-15 20:23:08 +0200

    mail

--- generator/random.py
+++ generator/random.py
@@ -5,3 +5,7 @@ def random_string(prefix, maxlen):
     # symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     symbols = string.ascii_letters + string.digits + " "
     return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_name(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,5 +1,11 @@
+from generator.random import random_name
 
 def test_signup_new_account(app):
-    username = "user1"
+    username = random_name('user_', 10)
+    email = username + "@loalhost"
     password = "test"
     app.james.ensure_user_exists(username, password)
+    app.signup.new_user(username, email, password)
+    app.session.login(username, password)
+    assert app.session.is_logged_in_as(username)
+    app.session.logout()
